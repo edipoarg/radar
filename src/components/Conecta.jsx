619cb4d726572d205f405b0040e6de652f80fb7b
Niv Sardi <xaiki@evilgiggle.com>
2024-01-04 11:32:12 -0300

    move project description to conecta.jsx (with it's CSS)
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- src/components/Conecta.jsx
+++ src/components/Conecta.jsx
@@ -3,6 +3,15 @@ import './Conecta.css'
 import { Link as ScrollLink, animateScroll as scroll } from "react-scroll";
 import { useEffect } from 'react';
 
+export const Details = () => (
+    <>
+        <span id='negritaTitulo'>
+            RADAR (Registro de Ataques de Derechas Argentinas Radicalizadas)
+        </span>
+        es un proyecto periodístico colaborativo impulsado por el Equipo de Investigación Política (EdIPo) de la Revista Crisis con apoyo del Centro de Estudios Legales y Sociales (CELS) con el objetivo de fortalecer la democracia y la defensa de los derechos humanos.
+        Presentamos un mapeo colaborativo de hechos de violencia política protagonizados por derechas radicalizadas con el fin de contribuir al diagnóstico colectivo y la elaboración de estrategias de autocuidado.
+    </>
+)
 
 export default function reportaForm() {
     useEffect(() => {
@@ -21,8 +30,7 @@ export default function reportaForm() {
                 </div>
                 <div id='contectaTextContainter'>
                     <h6 id='conectaTextRadar'>
-                        RADAR es un proyecto periodístico colaborativo impulsado por el Equipo de Investigación Política (EdIPo) de la Revista Crisis con apoyo del Centro de Estudios Legales y Sociales (CELS) con el objetivo de fortalecer la democracia y la defensa de los derechos humanos.
-                        Presentamos un mapeo colaborativo de hechos de violencia política protagonizados por derechas radicalizadas con el fin de contribuir al diagnóstico colectivo y la elaboración de estrategias de autocuidado.
+                        <Details />
                     </h6>
 
                 </div>
