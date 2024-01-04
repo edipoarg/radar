595dddeddb813376e2f8389271f53c0c0a5dec77
anon author <noemail@nothing.null>
2024-01-04 18:03:27 -0300

    add slider component

---
+++ src/components/months-slider.jsx
@@ -0,0 +1,52 @@
+import PropTypes from "prop-types"
+import { useCallback } from "react";
+import { Slider } from "@mui/material";
+
+const valueLabelFormatByMonths = (totalMonths) => (value) => {
+    const diff = totalMonths - value;
+    const date = new Date()
+    date.setMonth(date.getMonth() - diff)
+    return `${date.getMonth() + 1}/${date.getFullYear()}`;
+};
+/** Agrega un botón o elemento para cambiar la visibilidad de Filtros */
+export default function MonthsSlider({ className, monthRange, setMonthRange, totalMonths, startDateLabel, endDateLabel }) {
+
+    // Uso useCallback para evitar que se re-declare la función anónima en cada render
+    const handleChange = useCallback((event) => {
+        setMonthRange(event.target.value);
+    }, [setMonthRange]);
+
+    // Esto da una warning en el linter porque no tiene información de tipado. La lista de dependencias es exhaustiva.
+    const valueLabelFormat = useCallback(valueLabelFormatByMonths(totalMonths), [totalMonths]);
+
+    return (<div className={className}>
+        <Slider
+            max={totalMonths}
+            valueLabelDisplay="auto"
+            value={monthRange}
+            step={1}
+            getAriaValueText={valueLabelFormat}
+            valueLabelFormat={valueLabelFormat}
+            onChange={handleChange}
+            aria-labelledby="non-linear-slider"
+        />
+        <div id='referenciasFechas'>
+            <div> <h6 id='fechaInicio'>{startDateLabel}</h6>  </div>
+            <div>  </div>
+            <div> <h6 id='fechaCierre'>{endDateLabel}</h6>  </div>
+        </div>
+    </div>)
+}
+
+MonthsSlider.propTypes = {
+    className: PropTypes.string,
+    monthRange: PropTypes.arrayOf(PropTypes.number).isRequired,
+    setMonthRange: PropTypes.func.isRequired,
+    totalMonths: PropTypes.number.isRequired,
+    startDateLabel: PropTypes.string.isRequired,
+    endDateLabel: PropTypes.string.isRequired,
+}
+
+MonthsSlider.defaultProps = {
+    className: "",
+}
