d2febad2618b54d934ca5dcc76edf9f9a932e73b
Juani Garay <jngaray93@gmail.com>
2024-01-25 12:12:52 -0300

    add typescript to Vite project

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,12 +3,14 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
     "prettier",
-    "plugin:prettier/recommended"
+    "plugin:prettier/recommended",
   ],
+  parser: '@typescript-eslint/parser',
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
