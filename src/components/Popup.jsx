d0f1b96df39da9a269c567517f4a3310ed8e9fa7
alexaiconejo <fondo.gif>
2024-01-26 17:05:50 -0300

    Closes #33 - delete tipo drom propTypes and const Popup

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,7 +1,7 @@
 import styles from "./Popup.module.css";
 import PropTypes from "prop-types";
 
-const Popup = ({ title, date, source, tipo }) => (
+const Popup = ({ title, date, source}) => (
   <div className={styles.popup}>
     <h3 className={styles.displayTitulo}>{title}</h3>
     <div className={styles.masDatosPopup}>
@@ -23,7 +23,6 @@ Popup.propTypes = {
   title: PropTypes.string.isRequired,
   date: PropTypes.instanceOf(Date).isRequired,
   source: PropTypes.string.isRequired,
-  tipo: PropTypes.array.isRequired,
 };
 
 export default Popup;
