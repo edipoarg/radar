81cf5bd51d91d6b4da398d769da16fffc76e085a
Juani Garay <jngaray93@gmail.com>
2024-02-04 17:46:58 -0300

    migrate Main2 component to ts

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { loader as getURLs } from "./components/Loader.jsx";
 import { createHashRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 
-import Main2 from "./components/Main2.jsx";
+import Main2 from "./components/Main2";
 import Conecta from "./components/Conecta";
 import Notas from "./components/Notas.jsx";
 import Reporta from "./components/Reporta";
