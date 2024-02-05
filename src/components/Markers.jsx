8ee7f761eac8b2602348a5d722bb2b07f83a3f53
anon author <noemail@nothing.null>
2024-02-05 15:52:03 -0300

    markers: FIXUP: remove old comment
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- src/components/Markers.jsx
+++ src/components/Markers.jsx
@@ -30,7 +30,6 @@ export const Markers = ({
 
   const renderMarker = (event) => {
     const { coords, title, date, source, id, tipoId } = event;
-    // Document this: Where does 'hovered' come from? It's not in the stylesheet
     const markerStyle = `${styles.circulo} ${id === selected ? styles.selected : ""} ${tipoIdStyles[tipoId]}`;
 
     return (
