eece2988df51d08f9e04679cef74435ad9fede00
anon author <noemail@nothing.null>
2024-02-09 11:27:06 -0300

    migrate marquee component to ts

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import Marquee from "../components/Marquee.jsx";
+import Marquee from "../components/Marquee";
 import NavbarBootstrap from "../components/Navbar.jsx";
 
 export default function Root() {
