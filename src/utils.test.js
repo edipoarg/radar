2aa60fb550dd37b9e0b8f0cb7dd05df3a96b2c8b
Niv Sardi <xaiki@evilgiggle.com>
2024-02-19 10:49:55 -0300

    add tests for utils.js
    this test breaks as the patch included in:
    306517d69978539409b863403583ba4396d6752b     #34 - fix fetch - january
    is value 0, december 11 so i substract 1 from month newDate value on
    fetch
    
    is wrong.

---
+++ src/utils.test.js
@@ -0,0 +1,22 @@
+import { expect, it, describe } from "vitest";
+import { newDate, chomp /* mapChomp, Classifier  */ } from "./utils.js";
+
+describe("newDate", () => {
+  it("parses DD/MM/YYYY dates correctly", () => {
+    let d = newDate("10/12/1999");
+    expect(d).toEqual(new Date(1999, 12, 10));
+  });
+  ["10/12/19", "10/12/1999/2", "12/14/1999"].map((d) =>
+    it(`error on ${d}`, () => {
+      let D = newDate(d);
+      expect(D).toBe(null);
+    }),
+  );
+});
+
+describe("chomp", () => {
+  it("should remove all trailing and preceding space", () => {
+    expect(chomp(" lol")).toBe("lol");
+    expect(chomp("lol ")).toBe("lol");
+  });
+});
