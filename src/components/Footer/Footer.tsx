16d54083e16e220c0560bf0ed9bc9b6be8d66926
Juani Garay <jngaray93@gmail.com>
2024-08-01 16:31:47 -0300

    Add current project version to footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,20 +1,21 @@
 import styles from "./Footer.module.css";
 import Navlinks from "../../routes/index.js";
 
+const currentVersion = import.meta.env.VITE_RADAR_VERSION;
+
 export default function Footer() {
   return (
     <div className={styles.footer}>
-      <div></div>
-      <div className={styles.desarrollo}>
-        <h6 className={styles.desarrolladoX}>
+      {currentVersion && (
+        <span className={styles.version}>Ra-dar {currentVersion} / </span>
+      )}
+      <h6 className={styles.desarrolladoX}>
+        Desarrollado x{" "}
+        <a className={styles.edipo} href={Navlinks.edipo}>
           {" "}
-          Desarrollado x{" "}
-          <a className={styles.edipo} href={Navlinks.edipo}>
-            {" "}
-            [ EdIPo ]{" "}
-          </a>
-        </h6>
-      </div>
+          [ EdIPo ]{" "}
+        </a>
+      </h6>
     </div>
   );
 }
