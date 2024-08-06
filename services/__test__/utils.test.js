3a7638f92413e02812bae94e56e70f12e82a4d5e
anon author <noemail@nothing.null>
2024-08-06 17:04:24 -0300

    fix date parsing

--- services/__test__/utils.test.js
+++ services/__test__/utils.test.js
@@ -1,25 +1,20 @@
 import { expect, it, describe } from "vitest";
-import { newDate, chomp, mapChomp, Classifier } from "../utils.cjs";
+import { parseTsvDateToJsDate, mapChomp, Classifier } from "../utils.cjs";
 
-describe("newDate", () => {
+describe("parseTsvDateToJsDate", () => {
   it("parses DD/MM/YYYY dates correctly", () => {
-    let d = newDate("10/12/1999");
-    expect(d).toEqual(new Date(1999, 12, 10));
+    let d = parseTsvDateToJsDate("10/12/1999");
+    expect(d).toEqual(new Date(1999, 11, 10));
   });
 
   ["10/12/19", "10/12/1999/2", "12/14/1999"].map((d) =>
-    it(`errors on invalid date: ${d}`, () => {
-      let D = newDate(d);
-      expect(D).toBe(null);
+    describe(`with ${d}`, () => {
+      it(`errors on invalid date: ${d}`, () => {
+        let D = parseTsvDateToJsDate(d);
+        expect(D).toBe(null);
+      });
     }),
   );
-});
-
-describe("chomp", () => {
-  it("should remove all trailing and preceding spaces", () => {
-    expect(chomp(" lol")).toBe("lol");
-    expect(chomp("lol ")).toBe("lol");
-  });
 });
 
 describe("mapChomp", () => {
