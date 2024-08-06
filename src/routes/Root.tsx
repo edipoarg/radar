1740436382824e12893273f4416d04d83369db05
anon author <noemail@nothing.null>
2024-08-06 09:36:24 -0300

    remove marquee, and its supplementary styles and components (#140)
    Co-authored-by: Juani Garay <jngaray93@gmail.com>

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,12 +1,10 @@
 import { Outlet } from "react-router-dom";
 import NavbarBootstrap from "../components/Navbar/Navbar";
-import Marquee from "../components/Marquee/Marquee";
 
 export default function Root() {
   return (
     <>
       <NavbarBootstrap /> {/* Navbar siempre se muestra */}
-      <Marquee />
       <Outlet />
     </>
   );
