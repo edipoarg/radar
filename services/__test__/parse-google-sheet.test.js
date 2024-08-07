eae81459e89509186cd198b5a272f14cdf6df4b2
Juani Garay <jngaray93@gmail.com>
2024-08-07 00:26:46 -0300

    rework google-sheets module

--- services/__test__/parse-google-sheet.test.js
+++ services/__test__/parse-google-sheet.test.js
@@ -1,10 +1,10 @@
 import { describe, expect, it } from "vitest";
-import { fetchTSV } from "../parse-tsv.cjs";
+import { parseTSVToJSON } from "../parse-tsv.cjs";
 import parsedCases from "./casos-parseados.json";
 
 describe("fetchTSV", () => {
   it("parses TSV to JSON as expected", async () => {
-    const result = await fetchTSV("services/__test__/sheet.tsv");
+    const result = await parseTSVToJSON("services/__test__/sheet.tsv");
     expect(JSON.stringify(result, null, 4)).toEqual(
       JSON.stringify(parsedCases, null, 4),
     );
