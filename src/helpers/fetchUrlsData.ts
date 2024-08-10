8e3da35256c37ddca46cbe79f3eeee44aab8b9e3
Juani Garay <jngaray93@gmail.com>
2024-08-10 14:34:16 -0300

    retype fetch urls data

--- src/helpers/fetchUrlsData.ts
+++ src/helpers/fetchUrlsData.ts
@@ -1,22 +1,22 @@
-function fetchSingleUrlData(url: string): Promise<string> {
+function fetchSingleUrlData<T>(url: string): Promise<T> {
   return fetch(url).then((r) => r.json());
 }
 
 export async function fetchUrlsData(
   urls: Record<string, string>,
-): Promise<Record<string, string>> {
+): Promise<Record<string, unknown>> {
   /* Convert URLs into an array of promises of tuples consisting of
    * on one hand, the key of the URL,
    * and on the other, the response to the URL itself
    */
   const keyAndResponsePromises = Object.entries(urls).map(
-    async ([key, url]): Promise<[string, string]> => [
+    async ([key, url]): Promise<[string, unknown]> => [
       key,
       await fetchSingleUrlData(url),
     ],
   );
   const keyAndResponses = await Promise.all(keyAndResponsePromises);
-  const urlsDataAccumulator: Record<string, string> = {};
+  const urlsDataAccumulator: Record<string, unknown> = {};
   keyAndResponses.forEach(([key, response]) => {
     urlsDataAccumulator[key] = response;
   });
