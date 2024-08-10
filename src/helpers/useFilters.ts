e699d3d1b29cf913f768d2737cb003445f025e25
anon author <noemail@nothing.null>
2024-08-10 14:51:00 -0300

    refactor useFilters to test its internal logic

--- src/helpers/useFilters.ts
+++ src/helpers/useFilters.ts
@@ -1,47 +1,45 @@
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import type { AttacksData, Case } from "../../common/json-shape";
 import { isCaseTipoId, type CaseTipoId } from "../types/caseData";
 
-type TipoFilter = Record<CaseTipoId, boolean>;
+type TipoFilters = Record<CaseTipoId, boolean>;
 
 type FiltersUtilities = {
   setDates: (dates: { min: number; max: number }) => void;
-  setTipoFilters: (setter: (oldFilters: TipoFilter) => TipoFilter) => void;
-  tipoFilters: TipoFilter;
+  setTipoFilters: (setter: (oldFilters: TipoFilters) => TipoFilters) => void;
+  tipoFilters: TipoFilters;
   filteredData: Case[];
 };
 
-export const useFilters = (attacksData: AttacksData): FiltersUtilities => {
-  const { cases, min, max } = attacksData;
+export const caseIsWithinMinAndMaxDatesWithDates =
+  (dates: { min: number; max: number }) => (someCase: Case) =>
+    someCase.date >= dates.min && someCase.date <= dates.max;
 
+export const caseIsAllowedByTipoFiltersWithFilters =
+  (filters: TipoFilters) => (someCase: Case) =>
+    someCase.tipoId
+      .filter(isCaseTipoId)
+      .some((individualTipo) => filters[individualTipo]);
+
+export const useFilters = ({
+  cases,
+  min,
+  max,
+}: AttacksData): FiltersUtilities => {
   const [dates, setDates] = useState({ min, max });
-  const isWithinMinAndMaxDates = useCallback(
-    (someCase: Case) =>
-      someCase.date >= dates.min && someCase.date <= dates.max,
-    [dates.min, dates.max],
-  );
-
   const [tipoFilters, setTipoFilters] = useState({
     t1: true,
     t2: true,
     t3: true,
   });
-  const isAllowedByTipoFilters = useCallback(
-    (someCase: Case) =>
-      someCase.tipoId
-        .filter(isCaseTipoId)
-        .some((individualTipo) => tipoFilters[individualTipo]),
-    [tipoFilters],
-  );
-
   const [filteredData, setFilteredData] = useState<Case[]>(cases);
 
   useEffect(() => {
     const newData = cases
-      .filter(isWithinMinAndMaxDates)
-      .filter(isAllowedByTipoFilters);
+      .filter(caseIsWithinMinAndMaxDatesWithDates(dates))
+      .filter(caseIsAllowedByTipoFiltersWithFilters(tipoFilters));
     setFilteredData(newData);
-  }, [cases, isAllowedByTipoFilters, isWithinMinAndMaxDates]);
+  }, [cases, dates, tipoFilters]);
 
   return {
     setDates,
