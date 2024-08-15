1de3dbf3066429704fc423d6f95d70e22067db3c
Juani Garay <jngaray93@gmail.com>
2024-08-14 21:30:10 -0300

    simplify Filtros general layout

--- src/components/Filtros/Filtros.tsx
+++ src/components/Filtros/Filtros.tsx
@@ -26,58 +26,51 @@ export default function Filtros({
 
   return (
     <div className={styles.filtros}>
-      <div className={styles.boxes}>
-        <div className={styles.botonFiltros}>
-          <br />
-        </div>
-        <div className={styles.display}>
-          <h4>casos:</h4>
-          <h1 className={styles.cantCasos}>{caseCount}</h1>
-        </div>
-        <div>
-          <div className={styles.filtroTipo}>
-            <div className={styles.tipos}>
-              <div className={styles.tipo1Ref}>
-                {" "}
-                <div className={styles.tipo1Icon}></div>
-                <h4 className={styles.tipoClass}>
-                  Ataque a símbolos y lugares{" "}
-                </h4>
-              </div>
-              <Switch
-                defaultChecked={tipoFilters.t1}
-                onChange={handleTipoFilterChange("t1")}
-              ></Switch>
+      <header>
+        <h4>casos:</h4>
+        <h1 className={styles.cantCasos}>{caseCount}</h1>
+      </header>
+      <div>
+        <div className={styles.filtrosPorTipo}>
+          <div className={styles.tipos}>
+            <div className={styles.tipo1Ref}>
+              {" "}
+              <div className={styles.tipo1Icon}></div>
+              <h4 className={styles.tipoClass}>Ataque a símbolos y lugares </h4>
+            </div>
+            <Switch
+              defaultChecked={tipoFilters.t1}
+              onChange={handleTipoFilterChange("t1")}
+            ></Switch>
+          </div>
+
+          <div className={styles.tipos}>
+            <div className={styles.tipo2Ref}>
+              {" "}
+              <div className={styles.tipo2Icon}></div>
+              <h4 className={styles.tipoClass}>
+                Hostigamiento e intimidación{" "}
+              </h4>
             </div>
 
-            <div className={styles.tipos}>
-              <div className={styles.tipo2Ref}>
-                {" "}
-                <div className={styles.tipo2Icon}></div>
-                <h4 className={styles.tipoClass}>
-                  Hostigamiento e intimidación{" "}
-                </h4>
-              </div>
+            <Switch
+              defaultChecked={tipoFilters.t2}
+              onChange={handleTipoFilterChange("t2")}
+            ></Switch>
+          </div>
 
-              <Switch
-                defaultChecked={tipoFilters.t2}
-                onChange={handleTipoFilterChange("t2")}
-              ></Switch>
+          <div className={styles.tipos}>
+            <div className={styles.tipo3Ref}>
+              {" "}
+              <div className={styles.tipo3Icon}></div>
+              <h4 className={styles.tipoClass}>
+                Atentados contra la integridad física y la vida{" "}
+              </h4>
             </div>
-
-            <div className={styles.tipos}>
-              <div className={styles.tipo3Ref}>
-                {" "}
-                <div className={styles.tipo3Icon}></div>
-                <h4 className={styles.tipoClass}>
-                  Atentados contra la integridad física y la vida{" "}
-                </h4>
-              </div>
-              <Switch
-                defaultChecked={tipoFilters.t3}
-                onChange={handleTipoFilterChange("t3")}
-              ></Switch>
-            </div>
+            <Switch
+              defaultChecked={tipoFilters.t3}
+              onChange={handleTipoFilterChange("t3")}
+            ></Switch>
           </div>
         </div>
       </div>
