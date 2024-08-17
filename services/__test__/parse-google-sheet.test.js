f610c5186b9c171e10033e21fbd97fb9e31cdd05
anon author <noemail@nothing.null>
2024-08-17 11:52:31 -0300

    update tests to new spec

--- services/__test__/parse-google-sheet.test.js
+++ services/__test__/parse-google-sheet.test.js
@@ -5,8 +5,6 @@ import parsedCases from "./casos-parseados.json";
 describe("fetchTSV", () => {
   it("parses TSV to JSON as expected", async () => {
     const result = await parseTSVToJSON("services/__test__/sheet.tsv");
-    expect(JSON.stringify(result, null, 4)).toEqual(
-      JSON.stringify(parsedCases, null, 4),
-    );
+    expect(result).toEqual(parsedCases);
   });
 });
