94ed1204fe999f008b2c87e0cf9f4216a311af69
Juani Garay <jngaray93@gmail.com>
2024-08-18 14:51:42 -0300

    Make navbar responsive

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,10 +1,10 @@
 import { Outlet } from "react-router-dom";
-import NavbarBootstrap from "../components/Navbar/Navbar";
+import Navbar from "../components/Navbar/Navbar";
 
 export default function Root() {
   return (
     <>
-      <NavbarBootstrap /> {/* Navbar siempre se muestra */}
+      <Navbar /> {/* Navbar siempre se muestra */}
       <Outlet />
     </>
   );
