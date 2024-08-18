d276f67d8f51678468d1549a63d2ac7057cb6af3
anon author <noemail@nothing.null>
2024-08-18 17:51:41 -0300

    simplify landing and investigations

--- src/components/Notas/Notas.tsx
+++ src/components/Notas/Notas.tsx
@@ -1,24 +1,17 @@
-import "bootstrap/dist/css/bootstrap.min.css";
 import styles from "./Notas.module.css";
-import "./NotasGlobalStyles.css";
-import { Carousel } from "react-bootstrap";
 import { Button } from "../Button/Button";
 import notas from "../../notas.json";
 
 export default function Notas() {
   return (
-    <div className={styles.notas}>
-      <Carousel className={styles.notasContainer}>
-        {notas.map(({ title, author, link }, i) => (
-          <Carousel.Item key={i}>
-            <div className={styles.notasItemContainer}>
-              <h1>{title}</h1>
-              {author && <h4>Por {author}</h4>}
-              <Button to={link}>ver</Button>
-            </div>
-          </Carousel.Item>
-        ))}
-      </Carousel>
-    </div>
+    <section className={styles.notas}>
+      {notas.map(({ title, author, link }, i) => (
+        <article key={i} className={styles.nota}>
+          <h1>{title}</h1>
+          {author && <h4>Por {author}</h4>}
+          <Button to={link}>ver</Button>
+        </article>
+      ))}
+    </section>
   );
 }
