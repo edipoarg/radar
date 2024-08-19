dfd5a2d55d0f4aba0b8b55018401e5ff76184a87
anon author <noemail@nothing.null>
2024-08-19 19:10:00 -0300

    add fetching of remote color-by-type configuration

--- services/constants.js
+++ services/constants.js
@@ -1,11 +1,17 @@
 import "process";
 const tsvId = process.env.CASES_SPREADSHEET_ID;
+const colorsByTypeId = process.env.COLORS_BY_TYPE_SPREADSHEET_ID;
 
 if (tsvId == undefined)
   throw new Error("CASES_SPREADSHEET_ID environment variable not provided");
+if (colorsByTypeId == undefined)
+  throw new Error(
+    "COLORS_BY_TYPE_SPREADSHEET_ID environment variable not provided",
+  );
 
 const constants = {
   tsvUrl: `https://docs.google.com/spreadsheets/d/e/${tsvId}/pub?output=tsv`,
+  colorsByTipeTSVUrl: `https://docs.google.com/spreadsheets/d/${colorsByTypeId}/pub?gid=496295073&single=true&output=tsv`,
 };
 
 export default constants;
