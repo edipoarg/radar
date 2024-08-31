29e1cd655311149dd1286db961d9befe2707a1c3
anon author <noemail@nothing.null>
2024-08-31 13:27:37 -0300

    add toggle switch

--- src/components/Filters/ByType/TypeFilter/TypeFilter.tsx
+++ src/components/Filters/ByType/TypeFilter/TypeFilter.tsx
@@ -12,11 +12,12 @@ export default function Filter({ description, color, value, onChange }: Props) {
     <button
       className={`${styles.tipo} ${value ? "" : styles.deactivated}`}
       title={description}
-      style={{ backgroundColor: value ? color : "#b5d0e8" }} // TODO: change grey to actual color
+      style={{ backgroundColor: value ? color : "#b5d0e8" }}
       type="button"
       onClick={() => onChange(!value)}
     >
       <span className={styles.tipoLabel}>{description}</span>
+      <div className={styles.switch}></div>
     </button>
   );
 }
