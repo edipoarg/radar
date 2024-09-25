44a5abcc188ae18c857ad80188a119f4c4b9acb5
anon author <noemail@nothing.null>
2024-09-25 11:50:41 -0300

    Updates

---
+++ data/mapsData/index.js
@@ -0,0 +1,7 @@
+import provincias from "./provincias.json";
+import departamentos from "./departamentos-argentina.json";
+import departamentosBsAs from "./departamentos-argentina.json";
+import rutas from './rutas.json'
+
+
+export { provincias, departamentos, departamentosBsAs, rutas, };
