a170936f8bed112022cbde953c37966a9643da26
solsticiodeverano <talleresfinge@gmail.com>
2025-04-17 17:47:10 -0300

    Presentacion.tsx + module.css + presentacion.ts

--- src/components/QuienesSomos/Presentacion/Presentacion.tsx
+++ src/components/QuienesSomos/Presentacion/Presentacion.tsx
@@ -1,3 +1,41 @@
+import styles from "./Presentacion.module.css";
+import { presentacion } from "./presentacion";
+import { GiRadarSweep } from "react-icons/gi";
+
+
 export default function Presentacion() {
-  return <div>Presentacion</div>;
+  return (
+    <section className={styles.section}>
+      <div className={styles.header}>
+      <GiRadarSweep className={styles.headerIcon} />
+        <h2 className={styles.title}>{presentacion.title}</h2>
+      </div>
+
+      <div className={styles.content}>
+        {Object.entries(presentacion.terms).map(([termName, term]) => {
+          return (
+            <section key={termName} className={styles.term}>
+              <h3>{termName}</h3>
+              <ul>
+                {term.definitions.map((definition, index) => (
+                  <li key={index} className={styles.description}>
+                    {definition.paragraphs.map((paragraph) => (
+                      <p key={paragraph}>{paragraph}</p>
+                    ))}
+                  </li>
+                ))}
+              </ul>
+              <ul className={styles.list}>
+                {term.members.map((member) => (
+                  <li key={member} className={styles.member}>
+                    {member}
+                  </li>
+                ))}
+              </ul>
+            </section>
+          );
+        })}
+      </div>
+    </section>
+  );
 }
---
+++ src/components/QuienesSomos/Presentacion/presentacion.ts
@@ -0,0 +1,62 @@
+import type { DefinitionList } from "../definition-list";
+
+export const presentacion: DefinitionList = {
+  title: "Presentación",
+  terms: {
+    "¿De dónde proviene la información?": {
+      definitions: [
+        {
+          paragraphs: [
+            "La última década estuvo marcada por la expansión global de las ultraderechas. La ola de las derechas radicalizadas cobró cuerpo a partir de la llegada de Donald Trump a la presidencia de los Estados Unidos en 2017 y se confirmó con la victoria de Jair Bolsonaro a fines de 2018 en Brasil. El batacazo de Javier Milei en nuestro país quizás constituyó el inicio de una segunda oleada, esta vez más radical que la anterior.",
+            "Más allá del vertiginoso ascenso de los últimos años, la derecha radicalizada no surge por generación espontánea. Hunde sus raíces en una derecha “analógica” o 1.0 de larga data: reaccionarios, conservadores, activistas pro dictadura. . En los años previos al triunfo electoral de La Libertad Avanza, en Argentina podríamos distinguir al menos cuatro etapas de la ultraderecha:",
+          ],
+        },
+      ],
+      members: [],
+    },
+    "Descripción general del modelo de la base de datos:": {
+      definitions: [
+        {
+          paragraphs: [
+            "Para su sistematización, la información está organizada en una base de datos que releva información de la siguiente manera:",
+          ],
+        },
+      ],
+      members: [
+        "Fecha del hecho",
+        "Fuente",
+        "Descripción del hecho",
+        "Víctimas",
+        "Tipo de ataque",
+        "Modalidad de ataque",
+        "Componente de la violencia",
+        "Coordenadas geográficas",
+        "Provincia",
+        "Agresor(es)",
+        "Uso de armas",
+        "Denuncia/Judicialización del ataque",
+      ],
+    },
+    "Cómo se construyen las variables y las categorías": {
+      definitions: [
+        {
+          paragraphs: [
+            "Las perspectivas de RADAR acerca de las diversas problemáticas relativas al avance de las derechas radicalizadas y su pase del ataque virtual al campo físico informan y alimentan la base de datos.",
+            "Estas discusiones son dinámicas, como lo son las problemáticas abordadas, pero se trata no de cambios repentinos sino del resultado de procesos y debates en el campo de la investigación política.",
+            "Por lo tanto, la construcción de variables y categorías de análisis pretende ser un reflejo de ello y puede cambiar a lo largo del tiempo.",
+            "Por ejemplo, la categorización de la narrativa de odio contra medios de comunicación no existía y se agregó frente a un contexto de ataques continuos contra los periodistas y trabajadores de prensa, promovidos por grupos de derecha radicalizada.",
+            "Algo similar sucede en el caso de la clasificación de los hechos registrados por la base, que obedece a una tipología de casos establecida en función de ataques geocalizables.",
+            "Las investigaciones llevadas adelante por el equipo de trabajo de RADAR desde el establecimiento de la base de datos en julio de 2023 han echado luz sobre nuevas formas en que estos hechos ocurren, lo cual finalmente ha dado lugar a un nuevo subsistema de categorización de los hechos.",
+            "Dicho subsistema tiene como núcleo una manera de distinguir problemáticas que es transversal a la tipología principal de hechos, y pretende contrarrestar algunas limitaciones identificadas en aquel anterior sistema de categorías sin tener que cambiarlas completamente, lo que hubiera producido un corte en la serie temporal.",
+            "Por ejemplo, en un principio se buscaba darle visibilidad exclusivamente a la violencia que pasaba a las acciones directas físicas.",
+            'Sin embargo, más adelante observamos el crecimiento de una modalidad de ataque que tiene su origen en lo virtual pero busca explícitamente promover acciones que traspasan la pantalla: el "doxeo".',
+            "Esta práctica, ya desplegada ampliamente en otras partes del mundo, desembarcó con fuerza en las redes sociales de Argentina (específicamente en Twitter, ahora X), siendo ejecutada mayormente por militantes de ultraderecha para vulnerar, desacreditar y silenciar a distintas personas, marcadas como objetivo por su identidad política o partidaria.",
+            "A partir de la irrupción de los doxeos en el ecosistema de violencia política decidimos ampliar nuestro registro para poder abordarlos en nuestro análisis.",
+            "Actualmente, hemos desarrollado una investigación sobre el tema. A su vez, estamos trabajando para próximamente compartir en nuestra plataforma información sobre prevención y primeros auxilios ante este tipo de ataques.",
+          ],
+        },
+      ],
+      members: [],
+    },
+  },
+};
