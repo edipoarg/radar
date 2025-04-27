5b219e78a21832736166561fdc0a5b916836da03
tisifone <noname@anonymous.gov>
2025-04-27 16:28:20 -0300

    fix and simplify current route styling

--- src/components/Navbar/DesktopNavbar/DesktopNavbar.tsx
+++ src/components/Navbar/DesktopNavbar/DesktopNavbar.tsx
@@ -1,4 +1,4 @@
-import { NavLink, useLocation } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import styles from "./DesktopNavbar.module.css";
 import Navlinks from "../../../routes/index";
 import { IoMdPin } from "react-icons/io";
@@ -6,25 +6,12 @@ import { TiInfoLarge } from "react-icons/ti";
 import { HiOutlineBellAlert } from "react-icons/hi2";
 import { LuFileSearch } from "react-icons/lu";
 
-const navlinkIsSelected =
-  (pathname: string) =>
-  (routeOfNavlink: (typeof Navlinks)[keyof typeof Navlinks]): boolean =>
-    pathname == routeOfNavlink;
-
-const getSelectedClassname = (isSelected: boolean) =>
-  isSelected ? styles.selected : "";
-
 const iconsHeight = "9vh 1vh";
 
 export default function DesktopNavbar() {
-  const { pathname } = useLocation();
-  const isSelected = navlinkIsSelected(pathname);
   return (
     <div className={styles.heading}>
-      <NavLink
-        to={Navlinks.home}
-        className={getSelectedClassname(isSelected(Navlinks.home))}
-      >
+      <NavLink to={Navlinks.home}>
         <IoMdPin size={iconsHeight} />
         Casos
       </NavLink>
@@ -32,10 +19,7 @@ export default function DesktopNavbar() {
         <TiInfoLarge size={iconsHeight} />
         ¿Quiénes somos?
       </NavLink>
-      <NavLink
-        to={Navlinks.investigaciones}
-        className={getSelectedClassname(isSelected(Navlinks.investigaciones))}
-      >
+      <NavLink to={Navlinks.investigaciones}>
         <LuFileSearch size={iconsHeight} />
         Investigaciones
       </NavLink>
--- src/components/Navbar/MobileNavbar/MobileNavbar.tsx
+++ src/components/Navbar/MobileNavbar/MobileNavbar.tsx
@@ -1,4 +1,4 @@
-import { NavLink, useLocation } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import styles from "./MobileNavbar.module.css";
 import Navlinks from "../../../routes/index";
 import { IoMdPin } from "react-icons/io";
@@ -8,14 +8,6 @@ import { LuFileSearch } from "react-icons/lu";
 import LogotipoRadar from "./LogotipoRadar.svg?react";
 import IsotipoRadar from "./IsotipoRadar.svg?react";
 
-const navlinkIsSelected =
-  (pathname: string) =>
-  (routeOfNavlink: (typeof Navlinks)[keyof typeof Navlinks]): boolean =>
-    pathname == routeOfNavlink;
-
-const getSelectedClassname = (isSelected: boolean) =>
-  isSelected ? styles.selected : "";
-
 type Props = {
   heightInVh: number;
 };
@@ -23,8 +15,6 @@ type Props = {
 const iconsHeight = "9vh 1vh";
 
 export default function MobileNavbar({ heightInVh }: Props) {
-  const { pathname } = useLocation();
-  const isSelected = navlinkIsSelected(pathname);
   return (
     <div className={styles.heading} style={{ height: `${heightInVh}vh` }}>
       <NavLink
@@ -35,19 +25,13 @@ export default function MobileNavbar({ heightInVh }: Props) {
         <LogotipoRadar />
       </NavLink>
       <div className={styles.pagesNavLinks}>
-        <NavLink
-          to={Navlinks.home}
-          className={getSelectedClassname(isSelected(Navlinks.home))}
-        >
+        <NavLink to={Navlinks.home}>
           <IoMdPin size={iconsHeight} />
         </NavLink>
         <NavLink to={Navlinks.quienesSomos}>
           <TiInfoLarge size={iconsHeight} />
         </NavLink>
-        <NavLink
-          to={Navlinks.investigaciones}
-          className={getSelectedClassname(isSelected(Navlinks.investigaciones))}
-        >
+        <NavLink to={Navlinks.investigaciones}>
           <LuFileSearch size={iconsHeight} />
         </NavLink>
         <NavLink to={Navlinks.novedades}>
@@ -56,7 +40,7 @@ export default function MobileNavbar({ heightInVh }: Props) {
         <NavLink
           title="Isotipo de Ra-dar"
           to={Navlinks.redRadar}
-          className={`${getSelectedClassname(isSelected(Navlinks.redRadar))} ${styles.isotypeImg}`}
+          className={`${styles.isotypeImg}`}
         >
           <IsotipoRadar />
         </NavLink>
