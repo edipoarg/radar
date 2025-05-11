80e017e7d296b53a9ff798822a649ff3c041e5de
tisifone <noname@anonymous.gov>
2025-05-11 14:02:57 -0300

    add 'red' route to desktop navbar

--- src/components/Navbar/DesktopNavbar/DesktopNavbar.tsx
+++ src/components/Navbar/DesktopNavbar/DesktopNavbar.tsx
@@ -1,13 +1,13 @@
 import { NavLink } from "react-router-dom";
 import styles from "./DesktopNavbar.module.css";
 import Navlinks from "../../../routes/index";
-import { IoMdPin } from "react-icons/io";
 import { TiInfoLarge } from "react-icons/ti";
 import { HiOutlineBellAlert } from "react-icons/hi2";
 import { LuFileSearch } from "react-icons/lu";
-import LogotipoRadar from "./LogotipoRadar.svg?react";
 import ReactPopup from "reactjs-popup";
 import { ReportPopupContent } from "../../ReportPopupContent/ReportPopupContent";
+import LogotipoRadar from "./LogotipoRadar.svg?react";
+import IsotipoRadar from "./IsotipoRadar.svg?react";
 
 const iconsHeight = "9vh 1vh";
 
@@ -29,6 +29,14 @@ export default function DesktopNavbar() {
         <NavLink to={Navlinks.novedades}>
           <HiOutlineBellAlert size={iconsHeight} />
           Novedades
+        </NavLink>
+        <NavLink
+          title="Isotipo de Ra-dar"
+          to={Navlinks.redRadar}
+          className={`${styles.isotypeImg}`}
+        >
+          <IsotipoRadar />
+          Red
         </NavLink>
       </div>
       <ReactPopup
