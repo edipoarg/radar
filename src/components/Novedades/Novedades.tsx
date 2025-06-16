45774e13634bab615fe28c0765a64f8ef1164b0b
tisifone <noname@anonymous.gov>
2025-06-16 11:44:32 -0300

    simplificar componente de Novedades

--- src/components/Novedades/Novedades.tsx
+++ src/components/Novedades/Novedades.tsx
@@ -1,5 +1,4 @@
 import styles from "./Novedades.module.css";
-import { novedades } from "./novedades";
 import { HiOutlineBellAlert } from "react-icons/hi2";
 import { LuMailPlus } from "react-icons/lu";
 
@@ -8,29 +7,28 @@ export default function Novedades() {
     <section className={styles.section}>
       <div className={styles.header}>
         <HiOutlineBellAlert className={styles.headerIcon} />
-        <h2 className={styles.title}>{novedades.title}</h2>
+        <h2 className={styles.title}>Novedades</h2>
       </div>
 
       <div className={styles.content}>
-        {Object.entries(novedades.terms).map(([termName]) => {
-          return (
-            <section key={termName} className={styles.term}>
-              <h3>{termName}</h3>
+        <section className={styles.term}>
+          <h3>
+            Suscribite al newsletter de RA-DAR para recibir novedades de nuestro
+            registro e investigaciones.
+          </h3>
 
-              <a
-                href="https://docs.google.com/forms/d/e/1FAIpQLSemLdPC4Fnm1NY8ScNG8noC_pvTIV1bvaVq4sw9dTrjHPT84Q/viewform"
-                target="_blank"
-                rel="noopener noreferrer"
-                className={styles.newsletterLink}
-              >
-                <section className={styles.solidButton}>
-                  <LuMailPlus className={styles.mailIcon} />
-                  <span className={styles.ingresarText}>Sumate</span>
-                </section>
-              </a>
+          <a
+            href="https://docs.google.com/forms/d/e/1FAIpQLSemLdPC4Fnm1NY8ScNG8noC_pvTIV1bvaVq4sw9dTrjHPT84Q/viewform"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.newsletterLink}
+          >
+            <section className={styles.solidButton}>
+              <LuMailPlus className={styles.mailIcon} />
+              <span className={styles.ingresarText}>Sumate</span>
             </section>
-          );
-        })}
+          </a>
+        </section>
       </div>
 
       <div className={styles.deco}></div>
