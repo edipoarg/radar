59aa66b1c0740e604bbf0f0af401ab79f34865ca
tisifone <noname@anonymous.gov>
2025-06-19 18:42:23 -0300

    optimizar imagenes desde el build

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 /// <reference types="vitest" />
 
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import svgr from 'vite-plugin-svgr' 
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),svgr()],
+  plugins: [react(),svgr(), ViteImageOptimizer()],
   base: "./",
   test: {
     globals: true,
