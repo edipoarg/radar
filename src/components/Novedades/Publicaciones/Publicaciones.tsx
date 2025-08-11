20af4b8cb86a848d7482df3751ccd74deae97b1b
tisifone <noname@anonymous.gov>
2025-08-11 19:30:47 -0300

    agrego newsletters para desktop

---
+++ src/components/Novedades/Publicaciones/Publicaciones.tsx
@@ -0,0 +1,108 @@
+import informe1 from "./Informe-1.md?raw";
+import informe2 from "./Informe-2.md?raw";
+import newsletter1 from "./Newsletter-1.md?raw";
+import newsletter2 from "./Newsletter-2.md?raw";
+import newsletter3 from "./Newsletter-3.md?raw";
+import newsletter4 from "./Newsletter-4.md?raw";
+import ReactMarkdown from "react-markdown";
+import styles from "./Publicaciones.module.css";
+
+type Publicacion = {
+  id: string;
+  titulo: string;
+  fecha: string; // YYYYMMDD
+  texto: string; // En Markdown
+};
+
+type Publicaciones = Publicacion[];
+
+const publicaciones: Publicaciones = [
+  {
+    fecha: "2023-12-01",
+    id: "Informe 1 - 2023",
+    texto: informe1,
+    titulo: "Informe 1 - 2023",
+  },
+  {
+    fecha: "2024-07-01",
+    id: "Informe 2 - 2024",
+    texto: informe2,
+    titulo: "Informe 2 - 2024",
+  },
+  {
+    fecha: "2023-11-01",
+    id: "Newsletter RADAR Nro. 1- Noviembre 2023",
+    texto: newsletter1,
+    titulo: "Newsletter RADAR Nro. 1- Noviembre 2023",
+  },
+  {
+    fecha: "2023-12-01",
+    id: "Newsletter RADAR Nro. 2- Diciembre 2023",
+    texto: newsletter2,
+    titulo: "Newsletter RADAR Nro. 2- Diciembre 2023",
+  },
+  {
+    fecha: "2024-03-01",
+    id: "Newsletter RADAR Nro. 3- Marzo 2024",
+    texto: newsletter3,
+    titulo: "Newsletter RADAR Nro. 3- Marzo 2024",
+  },
+  {
+    fecha: "2025-08-04",
+    id: "¡Alerta RADAR! | Boletín N° 1/2025",
+    texto: newsletter4,
+    titulo: "¡Alerta RADAR! | Boletín N° 1/2025",
+  },
+];
+
+export default function Publicaciones() {
+  return (
+    <section className={styles.Publicaciones}>
+      <nav className={styles.desktopOnly}>
+        <h1>Nuestras publicaciones</h1>
+        <ul>
+          {publicaciones.map((publicacion) => (
+            // I hate doing this but HashRouter does not let me use a simple anchor by ID
+            <li key={publicacion.id}>
+              <button
+                className={styles.navButton}
+                type="button"
+                onClick={() => {
+                  document.getElementById(publicacion.id)?.scrollIntoView();
+                }}
+                title={publicacion.titulo}
+              >
+                <div className={styles.navButtonLight}></div>
+                <div className={styles.navButtonContent}>
+                  <h4>{publicacion.titulo}</h4>
+                  <p>
+                    {new Date(Date.parse(publicacion.fecha)).toLocaleDateString(
+                      "es-AR",
+                    )}
+                  </p>
+                </div>
+              </button>
+            </li>
+          ))}
+        </ul>
+      </nav>
+
+      <article>
+        <section className={styles.listaDePublicaciones}>
+          {publicaciones.map((publicacion) => (
+            <article
+              className={styles.publicacion}
+              id={publicacion.id}
+              key={publicacion.id}
+            >
+              <h3>{publicacion.titulo}</h3>
+              <div className={styles.cuerpoDePublicacion}>
+                <ReactMarkdown>{publicacion.texto}</ReactMarkdown>
+              </div>
+            </article>
+          ))}
+        </section>
+      </article>
+    </section>
+  );
+}
