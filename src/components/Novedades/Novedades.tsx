ce55ded3e49bec5c5e59aeddce2082cc29f0a44d
tisifone <noname@anonymous.gov>
2025-08-16 19:48:58 -0300

    newsletter para mobile

--- src/components/Novedades/Novedades.tsx
+++ src/components/Novedades/Novedades.tsx
@@ -1,5 +1,4 @@
 import styles from "./Novedades.module.css";
-import { HiOutlineBellAlert } from "react-icons/hi2";
 import { LuMailPlus } from "react-icons/lu";
 import Publicaciones from "./Publicaciones/Publicaciones";
 
@@ -11,31 +10,22 @@ export default function Novedades() {
       <div className={styles.deco}></div>
       <Publicaciones />
 
-      <div className={styles.header}>
-        <HiOutlineBellAlert className={styles.headerIcon} />
-        <h2 className={styles.title}>Novedades</h2>
-      </div>
+      <section className={styles.subscribePrompt}>
+        <h3>
+          Suscribite al newsletter de RA-DAR para recibir novedades de nuestro
+          registro e investigaciones.
+        </h3>
 
-      <div className={styles.content}>
-        <section className={styles.term}>
-          <h3>
-            Suscribite al newsletter de RA-DAR para recibir novedades de nuestro
-            registro e investigaciones.
-          </h3>
-
-          <a
-            href={newsletterUrl}
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.newsletterLink}
-          >
-            <section className={styles.solidButton}>
-              <LuMailPlus className={styles.mailIcon} />
-              <span className={styles.ingresarText}>Sumate</span>
-            </section>
-          </a>
-        </section>
-      </div>
+        <a
+          href={newsletterUrl}
+          target="_blank"
+          rel="noopener noreferrer"
+          className={styles.newsletterLink}
+        >
+          <LuMailPlus className={styles.mailIcon} />
+          <span className={styles.ingresarText}>Sumate</span>
+        </a>
+      </section>
     </section>
   );
 }
