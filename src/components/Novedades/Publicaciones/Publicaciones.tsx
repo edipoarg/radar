5cbfb78932c1f218d9e82b1b1621cfc50dedeaf1
anonuser1083 <anonuser1083@protonmail.com>
2025-09-17 13:30:51 +0000

    Cargado news6. Modificado novedades.tsx y creado newsletter-6.md

--- src/components/Novedades/Publicaciones/Publicaciones.tsx
+++ src/components/Novedades/Publicaciones/Publicaciones.tsx
@@ -3,6 +3,7 @@ import newsletter2 from "./Newsletter-2.md?raw";
 import newsletter3 from "./Newsletter-3.md?raw";
 import newsletter4 from "./Newsletter-4.md?raw";
 import newsletter5 from "./Newsletter-5.md?raw";
+import newsletter6 from "./Newsletter-6.md?raw";
 import alerta1 from "./alerta-radar-1.md?raw";
 import ReactMarkdown from "react-markdown";
 import styles from "./Publicaciones.module.css";
@@ -18,28 +19,22 @@ type Publicaciones = Publicacion[];
 
 const publicaciones: Publicaciones = [
   {
+    fecha: "09/2025",
+    id: "Newsletter RADAR Nro. 3 - Septiembre 2025",
+    texto: newsletter6,
+    titulo: "Newsletter RADAR Nro. 3 - Septiembre 2025",
+  },
+  {
     fecha: "08/2025",
     id: "alerta radar N1 (4 de agosto de 2025)",
     texto: alerta1,
     titulo: "¡Alerta RADAR! | Boletín N° 1/2025",
   },
   {
-    fecha: "11/2023",
-    id: "Newsletter RADAR Nro. 1- Noviembre 2023",
-    texto: newsletter1,
-    titulo: "Newsletter RADAR Nro. 1- Noviembre 2023",
-  },
-  {
-    fecha: "12/2023",
-    id: "Newsletter RADAR Nro. 2- Diciembre 2023",
-    texto: newsletter2,
-    titulo: "Newsletter RADAR Nro. 2- Diciembre 2023",
-  },
-  {
-    fecha: "03/2024",
-    id: "Newsletter RADAR Nro. 3- Marzo 2024",
-    texto: newsletter3,
-    titulo: "Newsletter RADAR Nro. 3- Marzo 2024",
+    fecha: "08/2025",
+    id: "Newsletter RADAR Nro. 2 - Agosto 2025",
+    texto: newsletter5,
+    titulo: "Newsletter RADAR Nro. 2 - Agosto 2025",
   },
   {
     fecha: "07/2025",
@@ -48,11 +43,23 @@ const publicaciones: Publicaciones = [
     titulo: "Newsletter RADAR Nro. 1 - Julio 2025",
   },
   {
-    fecha: "08/2025",
-    id: "Newsletter RADAR Nro. 2 - Agosto 2025",
-    texto: newsletter5,
-    titulo: "Newsletter RADAR Nro. 2 - Agosto 2025",
+    fecha: "03/2024",
+    id: "Newsletter RADAR Nro. 3 - Marzo 2024",
+    texto: newsletter3,
+    titulo: "Newsletter RADAR Nro. 3 - Marzo 2024",
   },
+  {
+    fecha: "12/2023",
+    id: "Newsletter RADAR Nro. 2 - Diciembre 2023",
+    texto: newsletter2,
+    titulo: "Newsletter RADAR Nro. 2 - Diciembre 2023",
+  },
+  {
+    fecha: "11/2023",
+    id: "Newsletter RADAR Nro. 1 - Noviembre 2023",
+    texto: newsletter1,
+    titulo: "Newsletter RADAR Nro. 1 - Noviembre 2023",
+  },  
 ];
 
 export default function Publicaciones() {
